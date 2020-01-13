e49668059e99e5d23f09bf76419d9d1906fe51bd
thepaulman <paul.mannarino@gmail.com>
2020-01-13 18:31:37 -0500

    Cleaning up comments/logging

--- seamless-health.js
+++ seamless-health.js
@@ -1,12 +1,9 @@
-/*
-  1. get restaurant names/address
-    - search for elements with class 'restaurant-name'
-  2. send request to NYC for health data
-  3. add new text next to restauraunt name to represent 'grade'
-*/
 let body = document.body;
 let timer = setInterval(waitForRestaurantData, 1000);
-// debugger
+/*
+  Seamless loads the restaurant data in a clean JSON object but we must wait for the object to be loaded to take advantage
+  TODO: Utilize background scripts to set an onLoad event listener
+*/
 function waitForRestaurantData() {
   if(body.getElementsByTagName('ghs-schema-place-action')[0]) {
     clearInterval(timer);
@@ -17,35 +14,23 @@ function waitForRestaurantData() {
 
 function setRestaurantData() {
   restaurantData = JSON.parse(body.getElementsByTagName('ghs-schema-place-action')[0].firstChild.innerText);
-  let address = restaurantData.address.streetAddress;
-  console.log(restaurantData.address);
-  let i = address.indexOf(' ');
-  let splits = [address.slice(0,i), address.slice(i+1)];
-  // console.log(splits);
-  console.log(restaurantData);
-  loadHealthRatings(splits[0], splits[1].toUpperCase(), 
-  restaurantData.address.postalCode, 
-  restaurantData.geo.longitude, 
-  restaurantData.geo.latitude, 
-  restaurantData.telephone,
-  restaurantData.name);
+  loadHealthRatings(restaurantData.name);
 }
 
-function loadHealthRatings(buildingNum, street, zipcode, long, lat, phone, dba) {
-  console.log(dba);
+function loadHealthRatings(dba) {
   $.ajax({
     url: "https://data.cityofnewyork.us/resource/43nn-pn8j.json",
     type: "GET",
     data: {
       "$limit" : 5000,
       "$$app_token" : "nycDataAppToken",
-      "dba": dba.toUpperCase()
+      "dba": dba.toUpperCase() // "Doing Business As"
     }
   }).done(function(data) {
     data.sort((a, b) => {
       return a.inspection_date < b.inspection_date;
     });
-    console.log(data);
+    
     let ratingColor = '';
     switch (data[0].grade) {
       case 'A':
@@ -60,7 +45,6 @@ function loadHealthRatings(buildingNum, street, zipcode, long, lat, phone, dba) 
       default:
         ratingColor = 'gray';
     }
-    // debugger
     let healthRating = document.getElementById('health-rating-text');
     if (healthRating) {
       healthRating.style.color = ratingColor;
@@ -72,9 +56,3 @@ function loadHealthRatings(buildingNum, street, zipcode, long, lat, phone, dba) 
     }
   });
 }
-
-// A $( document ).ready() block.
-// $( document ).ready(function() {
-//   debugger
-//   console.log( "ready!" );
-// });
